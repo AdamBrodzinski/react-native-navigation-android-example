79d642449d258a2c38560ed411563191d22120c0
Adam Brodzinski <adambrodzinski@gmail.com>
2016-08-23 12:17:37 -0700

    adds Example project source files

---
+++ src/screens/index.ios.js
@@ -0,0 +1,24 @@
+import { Navigation } from 'react-native-navigation';
+
+import FirstTabScreen from './FirstTabScreen';
+import SecondTabScreen from './SecondTabScreen';
+import ThirdTabScreen from './ThirdTabScreen';
+import PushedScreen from './PushedScreen';
+import StyledScreen from './StyledScreen';
+import ModalScreen from './ModalScreen';
+import LightBoxScreen from './LightBoxScreen';
+import NotificationScreen from './NotificationScreen'
+import SideMenu from './SideMenu';
+
+// register all screens of the app (including internal ones)
+export function registerScreens() {
+  Navigation.registerComponent('example.FirstTabScreen', () => FirstTabScreen);
+  Navigation.registerComponent('example.SecondTabScreen', () => SecondTabScreen);
+  Navigation.registerComponent('example.ThirdTabScreen', () => ThirdTabScreen);
+  Navigation.registerComponent('example.PushedScreen', () => PushedScreen);
+  Navigation.registerComponent('example.StyledScreen', () => StyledScreen);
+  Navigation.registerComponent('example.ModalScreen', () => ModalScreen);
+  Navigation.registerComponent('example.LightBoxScreen', () => LightBoxScreen);
+  Navigation.registerComponent('example.NotificationScreen', () => NotificationScreen);
+  Navigation.registerComponent('example.SideMenu', () => SideMenu);
+}
